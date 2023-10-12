16c7947368d52bf03a153a21d0f429e485abb231
ymkim97 <rladudaud97@naver.com>
2023-10-12 21:16:41 +0900

    ThreadLocal

---
+++ core_advanced/advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -0,0 +1,16 @@
+package hello.advanced;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
+
+@Configuration
+public class LogTraceConfig {
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
+}
