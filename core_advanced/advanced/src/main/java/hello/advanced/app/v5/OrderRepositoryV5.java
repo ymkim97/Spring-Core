77e8077a8f5469492d0986426786a9ad41233ffb
ymkim97 <rladudaud97@naver.com>
2023-10-14 18:30:32 +0900

    Template Callback Pattern

---
+++ core_advanced/advanced/src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
@@ -0,0 +1,35 @@
+package hello.advanced.app.v5;
+
+import org.springframework.stereotype.Repository;
+
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+
+@Repository
+public class OrderRepositoryV5 {
+
+	private final TraceTemplate template;
+
+	public OrderRepositoryV5(LogTrace logTrace) {
+		this.template = new TraceTemplate(logTrace);
+	}
+
+	public void save(String itemId) {
+
+		template.execute("OrderRepository.save()", () -> {
+			if (itemId.equals("ex")) {
+				throw new IllegalStateException("예외 발생!");
+			}
+			sleep(1000);
+			return null;
+		});
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
