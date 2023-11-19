e92f796a927a211fbcf9e12b2b5d3a0de0eafc92
ymkim97 <rladudaud97@naver.com>
2023-11-20 01:18:04 +0900

    Spring AOP trace, retry aop

--- core_advanced/aop/src/main/java/hello/aop/exam/ExamService.java
+++ core_advanced/aop/src/main/java/hello/aop/exam/ExamService.java
@@ -2,6 +2,7 @@ package hello.aop.exam;
 
 import org.springframework.stereotype.Service;
 
+import hello.aop.exam.annotation.Trace;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -10,6 +11,7 @@ public class ExamService {
 
 	private final ExamRepository examRepository;
 
+	@Trace
 	public void request(String itemId) {
 		examRepository.save(itemId);
 	}
--- core_advanced/aop/src/test/java/hello/aop/exam/ExamTest.java
+++ core_advanced/aop/src/test/java/hello/aop/exam/ExamTest.java
@@ -3,10 +3,14 @@ package hello.aop.exam;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+import hello.aop.exam.aop.RetryAspect;
+import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Import({TraceAspect.class, RetryAspect.class})
 @SpringBootTest
 class ExamTest {
 
